347b170704026093c1e027cb29f69f9bb52a26cc
shabana <shabana.bellary@cloudreach.com>
2022-07-21 15:42:07 +0530

    added route file and modified

--- route.tf
+++ route.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "internet_route_table" {
 }
 
 resource "aws_route_table" "nat_route_table" {
-  vpc_id = aws_vpc.lab.id
+  vpc_id = aws_vpc.lab-vpc.id
   route {
     cidr_block = "0.0.0.0/0" 
     gateway_id = aws_nat_gateway.nat_gw.id
